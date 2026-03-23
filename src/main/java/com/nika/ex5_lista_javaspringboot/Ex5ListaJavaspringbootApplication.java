53a4dc6f01e2e478f8b31b553b6fecc1fb2c9ffa
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2026-03-22 23:58:42 -0300

    first commit

---
+++ src/main/java/com/nika/ex5_lista_javaspringboot/Ex5ListaJavaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.nika.ex5_lista_javaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex5ListaJavaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex5ListaJavaspringbootApplication.class, args);
+	}
+
+}
