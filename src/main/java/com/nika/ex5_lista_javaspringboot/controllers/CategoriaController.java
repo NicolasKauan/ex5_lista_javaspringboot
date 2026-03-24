d81f0fc1a8eb10c9047ef5c52cded6d23ffc02f7
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2026-03-23 21:08:31 -0300

    Controllers

---
+++ src/main/java/com/nika/ex5_lista_javaspringboot/controllers/CategoriaController.java
@@ -0,0 +1,38 @@
+package com.nika.ex5_lista_javaspringboot.controllers;
+
+import com.nika.ex5_lista_javaspringboot.models.Categoria;
+import com.nika.ex5_lista_javaspringboot.services.CategoriaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+    private final CategoriaService service;
+
+    public CategoriaController(CategoriaService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Categoria criar(@RequestBody Categoria categoria) {
+        return service.salvar(categoria);
+    }
+
+    @GetMapping
+    public List<Categoria> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Categoria buscar(@PathVariable Long id) {
+        return service.buscar(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
+}
